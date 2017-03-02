56d6aab63bc7649f8badaf22d1e8b233f4a072ee
root <root@instance-2.c.pelagic-rig-160215.internal>
2017-03-02 00:43:02 +0000

    phpinfo.php file added copy Configuration.php -> configuration.php

---
+++ configuration.php
@@ -0,0 +1,10 @@
+<?php
+class Configuration {
+
+    const DB_HOST = 'localhost';
+    const DB_NAME = 'paa';
+    const DB_USERNAME = 'paa_user';
+    const DB_PASSWORD = 'T4rZx@k$';
+}
+ 
+?>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/paa/login.php');
+	header('Location: '.$uri.'/login.php');
 	exit;
 ?>
 Something is wrong with the WAMP installation :-(
